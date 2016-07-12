2a77e113da4ec8230b21bdf4feb98f81250f2bf6
Jim Lesperance <contact@jimlesperance.com>
2016-07-12 12:47:25 +0000

    WE add Bower for dependency management and will_paginate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstraps-sass
 //= require turbolinks
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ module Zips
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # bootstraps mongoid within applications -- like rails console
-    Mongoid.load!('./config/mongoid.yml')
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -20,6 +19,16 @@ module Zips
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+
+    # bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
+
+    # which default ORM are we using with scaffold
+    # add --orm none, mongoid, or active_record
+    # to rails generate cmd line to be specific
+    config.generators {|g| g.orm :active_record}
+    # config.generators {|g| g.orm :mongoid}
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
